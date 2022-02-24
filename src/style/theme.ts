e8a6569eb8aa6cb9f94a09d59ebe4aaa9507af90
jaimetrovoada <jaimetrovoada@gmail.com>
2022-02-24 14:34:42 +0800

    add scrollbar stylings

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -20,6 +20,23 @@ export const gruvbox = createTheme({
   typography: {
     fontFamily: 'Source Code Pro, monospace',
   },
+    components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '*::-webkit-scrollbar': {
+          width: '0.2em'
+        },
+        '*::-webkit-scrollbar-track': {
+          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ebdbb2',
+          outline: '1px solid #a89984',
+          borderRadius: "16px",
+        },
+      },
+    },
+  },
 })
 
 export const catppuccin = createTheme({
